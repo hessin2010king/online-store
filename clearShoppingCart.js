3ce50d30a9ad9d06e3809859813898c40d1bb5b4
hussien shokry <hessinshokry3@gmail.com>
2024-07-15 04:15:31 +0300

    organize project

--- clearShoppingCart.js
+++
@@ -1,9 +0,0 @@
-let retrievedCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
-function clearShoppingCart() {
-  localStorage.removeItem("shoppingCart");
-  shoppingCart = [];
-  numberItemsCart.innerHTML = shoppingCart.length;
-  showItems(shoppingCart);
-}
-
-export default clearShoppingCart;
